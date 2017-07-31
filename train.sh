85fc2dc22da573142cddbdbd089a4e179b7be042
wulinmenghuantejing <2395274664@qq.com>
2017-07-31 18:59:35 +0800

    SNRs detection

---
+++ train.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+/home/caf/Downloads/caffe/build/tools/caffe train --solver=/home/caf/face-detect/solver.prototxt #\
+#--weights=models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel \
+#bie ren xun lian hao de moxing zuo wei chu shi hua can shu
+#--gpu 0 
